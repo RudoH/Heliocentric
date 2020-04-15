44ee95c334b59a609a5ac9e801cc608f18c968ab
RudoH <rudohengst@gmail.com>
2020-04-15 19:17:51 -0400

    Initial Heliocentric Commit

---
+++ index.js
@@ -0,0 +1,8 @@
+// rgb opacity => rgb weather opacity
+
+let sun = document.createElement("div");
+sun.setAttribute(
+  "src",
+  "https://www.vhv.rs/dpng/d/0-2578_clip-art-sun-transparent-background-hd-png-download.png"
+);
+document.appendChild(sun);
